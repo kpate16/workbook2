e3c10a00915fe7a66a390f0f86ff271ebec3430e
Kanchan Pate <kpate@pgh.yearup.org>
2023-04-25 10:24:46 -0400

    Initial commit of js

---
+++ program.js
@@ -0,0 +1,18 @@
+// var firstName, lastName;
+// firstName = "Kanchan";
+// lastName = "Pate";
+// console.log("Hello world " + firstName + " " + lastName);
+
+var customerId = 16;
+console.log(customerId);
+var firstName = "Kanchan";
+var middleName = "Girish";
+var lastName = "Pate";
+var gender = "Female";
+console.log(firstName + " " + middleName + " " + lastName);
+VarDate = "24th April";
+console.log(VarDate);
+var dob = "12th Feb";
+var licenseNum = 1617;
+var policyNum = 123456;
+console.log(dob + " " + licenseNum + " " + policyNum);
