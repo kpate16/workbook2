22267af0033c05cb73cffdadf284a440dea3d160
Kanchan Pate <kpate@pgh.yearup.org>
2023-04-25 12:05:38 -0400

    use of rule 72

---
+++ exercises/mathscripts/rule_of_72.js
@@ -0,0 +1,12 @@
+var interestrate = 10;
+var num = 72;
+
+var years;
+years = 72 / interestrate;
+console.log(
+  "interest rate " +
+    interestrate +
+    " your savings account will be worth " +
+    years +
+    "Years"
+);
