cc682c1bfeb64d5e6297ea865eb86e119d749700
Kanchan Pate <kpate@pgh.yearup.org>
2023-04-25 11:31:03 -0400

    Maths calculation

---
+++ exercises/mathscripts/net_worth.js
@@ -0,0 +1,7 @@
+var assets = 50;
+var debt = 40;
+var networth = assets - debt;
+console.log("Your net worth is some-number " + networth);
+
+
+
---
+++ exercises/tips.js
@@ -0,0 +1,14 @@
+// sample inputs
+var foodCost = 79.25;
+var tax = 6.54;
+var tip = 12.0;
+
+// calculation
+var totalDue = foodCost + tax + tip;
+// output
+console.log("The total due is " + totalDue);
+
+console.log("Food cost is " + foodCost + " and tax is " + tax);
+console.log("Tip is " + tip);
+console.log("Total Due is" + totalDue);
+console.log("Tip is:"+tip.toFixed(2));
