ac9e81f1ef58e3a5a62f4fe71fe13b50c961e744
Kanchan Pate <kpate@pgh.yearup.org>
2023-04-26 10:13:36 -0400

    Practice programming

---
+++ demos/parse_string_to_num.js
@@ -0,0 +1,11 @@
+//declaring variable a and assigning a string to it
+"use strict"
+var a = "100";
+var b = parseInt(a);
+console.log("b is: "+b);
+
+var dataTypeOfAVariable= typeof(a);
+console.log(dataTypeOfAVariable);
+
+var dataTypeOfBVariable= typeof(b);
+console.log(dataTypeOfBVariable);
---
+++ exercises/mathscripts/unusual_operators.js
@@ -0,0 +1,22 @@
+
+
+//declare assign variable
+var count = 0;
+// count = count + 1;
+var updatedCount = count++;
+console.log(updatedCount);
+console.log(count);
+count--
+console.log(count);
+
+
+console.log(count);
+
+var onHand = 10;
+var qtyReceived = 6;
+// onHand = onHand + qtyReceived;
+onHand += qtyReceived;
+console.log(onHand);
+
+
+
