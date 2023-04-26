66c1f594f8ab39adf108fc541dc560c6a2c793aa
Kanchan Pate <kpate@pgh.yearup.org>
2023-04-26 10:50:41 -0400

    learning conditional statement

---
+++ demos/if_else.js
@@ -0,0 +1,15 @@
+var grade = "A";
+
+if (grade == "A") {
+  console.log("Ice cream sunday");
+} else {
+  console.log("No sunday for you!");
+}
+
+var testScore = 69;
+
+if (testScore >= 70) {
+  console.log("pass");
+} else {
+  console.log("fail");
+}
