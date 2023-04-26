8962e4d7d9a83026b8b77c7651feaa0776f1bd22
Kanchan Pate <kpate@pgh.yearup.org>
2023-04-26 14:54:21 -0400

    Formula added for lean year

--- exercises/ifscripts/gregorian_calendar.js
+++ exercises/ifscripts/gregorian_calendar.js
@@ -1,5 +1,5 @@
 function check_leapyear(year) {
-  if (year % 4 == 0) {
+  if (year % 4 == 0 && (year % 100 != 0||year % 400 == 0)) {
     console.log(year + " is a Leap year");
   } else {
     console.log(year + " is not a Leap year");
