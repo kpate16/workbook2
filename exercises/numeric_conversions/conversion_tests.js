cf3db5bbf808a5754895d3f5ce287c107c3e6e30
Kanchan Pate <kpate@pgh.yearup.org>
2023-04-26 12:14:44 -0400

    Exercise of page num 81

---
+++ exercises/numeric_conversions/conversion_tests.js
@@ -0,0 +1,46 @@
+var a = " 101.1 ";
+var b = "55";
+var c = "402 Stevens";
+var d = "Number 5 ";
+
+//ParseInt Method
+var a1 = parseInt(a);
+console.log(a1);
+
+var b1 = parseInt(b);
+console.log(b1);
+
+var c1 = parseInt(c);
+console.log(c1);
+
+var d1 = parseInt(d);
+console.log(d1);
+
+//Parse Float Method
+var a1 = parseFloat(a);
+console.log(a1);
+
+var b1 = parseFloat(b);
+console.log(b1);
+
+var c1 = parseFloat(c);
+console.log(c1);
+
+var d1 = parseFloat(d);
+console.log(d1);
+
+//Number
+a1 = Number(a);
+console.log(a1);
+b1 = Number(b);
+console.log(b1);
+c1 = Number(c);
+console.log(c1);
+d1 = Number(d);
+console.log(d1);
+
+//Unary Operator
+console.log(++a);
+console.log(++b);
+console.log(++c);
+console.log(++d);
