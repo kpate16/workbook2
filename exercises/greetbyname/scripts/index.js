e48976d316ea976fae980d7e6e5e9b5a53bb9675
Kanchan Pate <kpate@pgh.yearup.org>
2023-04-28 12:55:33 -0400

    Exercise Page no 127

--- exercises/greetbyname/scripts/index.js
+++ exercises/greetbyname/scripts/index.js
@@ -4,7 +4,6 @@ function init() {
   const btn = document.getElementById("greetBtn");
   btn.addEventListener("click", onGreetUserBtnClicked);
 }
-
 function onGreetUserBtnClicked(event) {
   var name = document.getElementById("nameField").value;
 
